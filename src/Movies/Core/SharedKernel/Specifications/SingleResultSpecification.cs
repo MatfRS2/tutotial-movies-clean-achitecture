bd01cca6eec4710a5c506ea2ec55a6c07b8a7427
Vladimir Filipovic <vladofilipovic@hotmail.com>
2022-12-15 09:10:22 +0100

    zasad koristim Specification iz ardalis nuget paketa

---
+++ src/Movies/Core/SharedKernel/Specifications/SingleResultSpecification.cs
@@ -0,0 +1,17 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+//using Ardalis.Specification;
+
+//namespace Movies.SharedKernel.Specifications;
+///// <inheritdoc cref="ISingleResultSpecification{T}"/>
+//public class SingleResultSpecification<T> : Specification<T>, ISingleResultSpecification<T>
+//{
+//}
+
+///// <inheritdoc cref="ISingleResultSpecification{T, TResult}"/>
+//public class SingleResultSpecification<T, TResult> : Specification<T, TResult>, ISingleResultSpecification<T, TResult>
+//{
+//}
